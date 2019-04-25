bc84cb8b29f500a6426eac78c96ce2bb1b69c4b0
Ross <rbunn1997@gmail.com>
2019-04-25 11:16:20 +0100

    Added comic  book detail view

--- src/ComicVookGallery/Controllers/ComicBookController.cs
+++ src/ComicVookGallery/Controllers/ComicBookController.cs
@@ -8,14 +8,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-      public ActionResult Detail()
-      {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
 
-            return Content("Hello from the comic book controler!");
-      }
+        public ActionResult Detail()
+        {
+            return View();
+        }
     }
 }
